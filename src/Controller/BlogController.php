889f4c85512a70eecf21f961b8d207fd7d847222
younes youssefi <assoufiy@hotmail.fr>
2018-11-10 17:27:54 +0100

    quete4symfony

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,29 @@
+<?php
+    // src/Controller/BlogController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class BlogController extends AbstractController
+{
+/**
+* Matches /blog exactly
+* @Route("/blog/{page}", requirements={"page"="\d+"}, methods={"GET"}, name="blog_list")
+*/
+    public function list($page)
+    {
+        return $this->render('blog/index.html.twig', ['page' => $page]);
+    }
+
+    /**
+     * @Route("blog/{slug}"), requirements={"slug"="[a-z|0-9|-]"}, methods={"GET"}, name="blog_show")
+     */
+
+    public function show($slug = "Article Sans Titre")
+    {
+    $slug = ucwords(str_replace("-", " ", $slug));
+        return $this->render('blog/show.html.twig', ['slug' => $slug]);
+    }
+}
